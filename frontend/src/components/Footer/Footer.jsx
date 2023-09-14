d8b446bff8c7f357e2d9cf77dbd8a61159679cdb
Alexisss_cn <nc4186624@gmail.com>
2023-09-14 17:14:35 +0200

    realizacion del footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,28 +1,51 @@
 import React from 'react'
-import logo from '../Nav/assets/logo-secre.jpg';
-
+import '../../css/Footer.css'
 const Footer = () => {
   return (
     <>
-    <div>footer</div>
-    <div class="foot1"> 
-        <div className="box">  
-            <a href='#'>
-            <img src='img/logo-secre.jpg' alt='logo-secre.jpg' />
-            </a>
+    <footer class="pie">
+    <div class="header">
+            <div class="logo">
+                <img src="logo.png" alt=""/>
+                <p class="logotipo">Secretaría de Ciencias, Tecnología y Políticas Educativas</p>
+            </div>
+        
         </div>
-        <div className="box">  
-        <h2>Sobre Nosotros</h2>
-        <div className="red social">  
-        <a href='#'></a>
-        <a href='#'></a>
-        <a href='#'></a>
-        <a href='#'></a>
+       
+        <div class="section">
+            <div>
+                <h2>CITLAM</h2>
+                <a href="#">Inico</a>
+                <a href="#">Contactate con nosotros</a>
+                <a href="#">Sobre nosotros</a>
+               
+            </div>
+            <div>
+                <h2>Contactos</h2>
+                <a href="#">citlamcyt@gmail.com</a>
+                <a href="#">11-4441-0798</a>
+        
+            </div>
+            <div>
+                <h2>Redes</h2>
+                <a href="#">Instagram</a>
+                <a href="#">Facebook</a>
+                <a href="#">Youtube</a>
+            </div>
+            <div>
+                <h2>Suscribete</h2>
+                <form action="">
+                    <input type="text" placeholder="Ingresa tu Email"/>
+                    <button type="submit">Suscribete</button>
+                </form>
+            </div>
         </div>
+        <div class="derechos">
+            <p>Copyright©20223 Plaza CIencia</p>
+            <p>Privacidad, politica, Terminos y Condiciones</p>
         </div>
-    </div>
+    </footer>
     </>
   )
 }
-
 export default Footer
